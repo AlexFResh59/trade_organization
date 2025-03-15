56ad89147b5e3fcddbd3f8379c86d012454b902f
AlexFresh59 <anchikov_2004@mail.ru>
2025-03-15 15:04:19 +0300

    мелкие правки

--- roles/manager.php
+++ roles/manager.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once 'includes/db.php';
+require_once '../includes/db.php';
 
 // Проверка роли
 if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'manager') {
--- roles/sales/customers.php
+++ roles/sales/customers.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once 'includes/db.php';
+require_once '../../includes/db.php';
 
 if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'sales') {
     header("Location: ../index.php");
